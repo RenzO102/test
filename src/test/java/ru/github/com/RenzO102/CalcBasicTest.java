ede4dd7c6a7722dd06021d2b6e0db66ded995d33
RenzO102 <stson102@mail.ru>
2022-10-03 16:20:15 +0500

    format code 1.1

--- src/test/java/ru/github/com/RenzO102/CalcBasicTest.java
+++ src/test/java/ru/github/com/RenzO102/CalcBasicTest.java
@@ -1,35 +1,30 @@
 package ru.github.com.RenzO102;
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(Parameterized.class)
 public class CalcBasicTest {
-    private static java.util.Arrays Arrays;
-@Parameterized.Parameters(name = "{index}: {0} + {1} = {2}")
-    public static Iterable<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {1, 2, 3},
-                {2, 6, 8},
-                {18, 18, 36},
-                {15, 1, 16},
-                {15, 3, 18},
-        });
-    }
-    @Parameterized.Parameter(0)
+    @Parameterized.Parameter()
     public int firstSummand;
     @Parameterized.Parameter(1)
     public int secondSummand;
     @Parameterized.Parameter(2)
     public int sum;
-    @Before
-    public void initCalculator() {
+
+    @Parameterized.Parameters(name = "{index}: {0} + {1} = {2}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{{1, 2, 3}, {2, 6, 8}, {18, 18, 36}, {15, 1, 16}, {15, 3, 18}});
     }
+
     @Test
     public void shouldBeTestParameterSum() {
-        assertThat("Should be a different amount",firstSummand + secondSummand,is(sum));
+        assertThat("Should be a different amount", firstSummand + secondSummand, is(sum));
     }
 }
--- src/test/java/ru/github/com/RenzO102/CalcTest.java
+++ src/test/java/ru/github/com/RenzO102/CalcTest.java
@@ -1,20 +1,24 @@
 package ru.github.com.RenzO102;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
 public class CalcTest {
-    private Calculator calc;
+
     @Before
     public void Calculator() {
-        calc = new Calculator();
     }
+
     @Test
     public void shouldTestSumTwoDigits() {
-        assertThat(3 - 2, is(1));
+        assertThat("Should be a different amount", 3 - 2, is(1));
     }
+
     @Test
     public void shouldTestDifferenceTwoDigits() {
-        assertThat(12 * 5, is(60));
+        assertThat("Should be a different amount", 12 * 5, is(60));
     }
 }
